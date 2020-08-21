c7a2ab14fae0c34ea30498b8b8cf6c00ce7e1072
Xiaoyi Liu <lxy878@gmail.com>
2020-08-21 14:13:16 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,12 +1,12 @@
 class BirdsController < ApplicationController
   def index
     birds = Bird.all
-    render json: birds, only: [:id, :name, :species]
+    render json: birds, except: [:created_at, :updated_at]
   end
 
   def show
     bird = Bird.find_by(id: params[:id])
-    render json: bird, only: [:id, :name, :species]
+    render json: bird, except: [:created_at, :updated_at]
   end
 
 end
